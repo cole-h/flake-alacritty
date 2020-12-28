a6f6c389181f12a92cbbcb8297fa5edc5e8bab6a
Cole Helbling <cole.e.helbling@outlook.com>
2020-12-28 09:40:02 -0800

    flake: update

--- .ci/srht-submit.sh
+++ .ci/srht-submit.sh
@@ -9,19 +9,23 @@ SECRET_ATTR="token"
 
 # less user-specific
 BUILD_HOST="https://builds.sr.ht"
-if which passrs ; then
-  TOKEN="$(passrs show "${SECRET_NAME}" | grep token | cut -d'=' -f2)"
+if which passrs >/dev/null 2>&1; then
+	set +x
+	TOKEN="$(passrs show "${SECRET_NAME}" | grep ${SECRET_ATTR} | cut -d'=' -f2)"
+	set -x
 else
-  echo "passrs not on path. Exiting."
-  exit 1
+	echo "passrs not on path. Exiting."
+	exit 1
 fi
 
 DATA="$(mktemp)"
 MANIFEST="$(jq -aRs . < "${DIR}/srht-job.yaml")"
 echo "{ \"tags\": [ \"flake-alacritty\" ], \"manifest\": ${MANIFEST} }" > "${DATA}"
 
+set +x
 curl \
-  -H "Authorization:token ${TOKEN}" \
-  -H "Content-Type: application/json" \
-  -d "@${DATA}" \
-  "${BUILD_HOST}/api/jobs"
+	-H "Authorization:token ${TOKEN}" \
+	-H "Content-Type: application/json" \
+	-d "@${DATA}" \
+	"${BUILD_HOST}/api/jobs"
+set -x
